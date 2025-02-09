e3b6b6002f348d67c67af241ec686656062727d6
Rui <75087533+chenrui1124@users.noreply.github.com>
2025-02-09 22:38:20 +0800

    feat: add Switch component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,3 @@
 export * from './button'
 export * from './horizontal-scrolling'
+export * from './switch'
---
+++ src/components/switch/Switch.vue
@@ -0,0 +1,34 @@
+<script lang="ts">
+export interface SwitchProps {
+  modelValue?: boolean
+  disabled?: boolean
+  uiDanger?: boolean
+}
+</script>
+
+<script lang="ts" setup>
+defineProps<Omit<SwitchProps, 'modelValue'>>()
+
+const modelValue = defineModel<SwitchProps['modelValue']>()
+</script>
+
+<template>
+  <label
+    :data-status="uiDanger ? 'error' : void 0"
+    :class="[
+      'group/switch relative inline-block h-7 w-12 cursor-pointer rounded-full bg-otl/24 transition duration-300 has-checked:bg-pri has-focus-visible:ring-3 has-focus-visible:ring-pri/48 has-disabled:cursor-not-allowed has-disabled:bg-dis has-disabled:opacity-48',
+      'before:absolute before:inset-0 before:rounded-[inherit] before:border before:border-solid before:border-otl before:transition before:duration-300 has-checked:before:border-transparent has-disabled:before:border-dis',
+      '*:top-1/2 *:left-3.5 has-checked:*:translate-x-5'
+    ]"
+  >
+    <input
+      type="checkbox"
+      :disabled
+      v-model="modelValue"
+      class="peer pointer-events-none absolute -m-2.5 inline-block size-5 appearance-none rounded-full bg-otl transition duration-300 ease-braking outline-none not-checked:scale-75 group-active/switch:scale-125 checked:bg-sur"
+    />
+    <span
+      class="pointer-events-none absolute -m-4.5 inline-block size-9 rounded-full transition duration-300 ease-braking group-hover/switch:bg-on-sur/12 group-hover/switch:peer-checked:bg-pri/12 peer-disabled:hidden"
+    ></span>
+  </label>
+</template>
