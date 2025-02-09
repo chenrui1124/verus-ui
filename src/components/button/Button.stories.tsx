e19506ddd00efccf80e8a806f7b15a0004689c66
Rui <75087533+chenrui1124@users.noreply.github.com>
2025-02-09 22:39:01 +0800

    chore: update tailwind & storybook config

--- src/components/button/Button.stories.tsx
+++ src/components/button/Button.stories.tsx
@@ -1,12 +1,25 @@
 import type { Meta, StoryObj } from '@storybook/vue3'
 
 import { fn } from '@storybook/test'
+import { fontWeightOptions, textTransformOptions, variantOptions } from '@/ts'
 import { VButton } from '.'
 
 const meta: Meta<typeof VButton> = {
   title: 'Components/Button',
   component: VButton,
-  argTypes: {},
+  argTypes: {
+    appendIcon: { control: false },
+    disabled: { control: 'boolean' },
+    icon: { control: false },
+    loading: { control: 'boolean' },
+    text: { control: 'text' },
+    uiBlock: { control: 'boolean' },
+    uiDanger: { control: 'boolean' },
+    uiFontWeight: { control: 'inline-radio', options: fontWeightOptions() },
+    uiRounded: { control: 'boolean' },
+    uiTextTransform: { control: 'inline-radio', options: textTransformOptions() },
+    uiVariant: { control: 'inline-radio', options: variantOptions() }
+  },
   args: {
     text: 'New app',
     uiVariant: 'tonal',
