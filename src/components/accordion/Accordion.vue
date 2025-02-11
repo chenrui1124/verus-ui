7414c767e577e68d1285011394f1dc4117a9dbcc
Rui <75087533+chenrui1124@users.noreply.github.com>
2025-02-11 22:39:47 +0800

    feat: Accordion
    - Add component: `Accordion`.
    - refactor basic component: `Icon`.

---
+++ src/components/accordion/Accordion.vue
@@ -0,0 +1,36 @@
+<script lang="ts">
+export interface AccordionProps {
+  danger?: boolean
+  icon?: string
+  title?: string
+}
+</script>
+
+<script lang="ts" setup>
+import { Icon } from '@/basic'
+
+defineProps<AccordionProps>()
+</script>
+
+<template>
+  <div
+    :data-status="danger ? 'error' : void 0"
+    class="box-border grid grid-cols-1 grid-rows-[min-content_0fr] rounded-v3 bg-pri-ctr transition-all duration-500 ease-braking has-checked:grid-rows-[min-content_1fr]"
+  >
+    <label
+      class="peer flex h-12 cursor-pointer items-center gap-3 rounded-[inherit] px-6 text-pri transition-all duration-500 ease-braking select-none hover:bg-pri/8 has-checked:h-14"
+    >
+      <Icon v-if="icon" :icon class="-ml-0.5 size-5" />
+      <span class="mb-px flex-1 text-base">{{ title }}</span>
+      <input
+        type="checkbox"
+        class="-mr-0.5 i-[fluent--chevron-right-24-regular] size-5 appearance-none text-inherit transition duration-500 ease-braking checked:rotate-90"
+      />
+    </label>
+    <div
+      class="overflow-y-hidden px-6 py-0 text-sm/7 text-on-pri-var transition-all duration-500 ease-braking peer-has-checked:pt-3 peer-has-checked:pb-6"
+    >
+      <slot></slot>
+    </div>
+  </div>
+</template>
