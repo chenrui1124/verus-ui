c8741cea54112c42669f1015a8c5f0151b8b2433
Rui <75087533+chenrui1124@users.noreply.github.com>
2025-02-13 21:14:51 +0800

    refactor(Tooltip): rewrite utils

--- src/utils/useHover.ts
+++ src/utils/useHover.ts
@@ -2,23 +2,30 @@ export const useHover = (() => {
   const startHooks: Set<Function> = new Set()
   const endHooks: Set<Function> = new Set()
 
-  document.body.addEventListener('mouseover', (evt: MouseEvent) => {
-    for (const hook of startHooks) hook(evt)
-  })
+  const onPointerover = (evt: PointerEvent) => startHooks.forEach(hook => hook(evt))
+  const onPointerout = (evt: PointerEvent) => endHooks.forEach(hook => hook(evt))
 
-  document.body.addEventListener('mouseout', (evt: MouseEvent) => {
-    for (const hook of endHooks) hook(evt)
-  })
-
-  return (hook: { start?: (evt: MouseEvent) => void; end?: (evt: MouseEvent) => void }) => {
-    if (hook.start) startHooks.add(hook.start)
-    if (hook.end) endHooks.add(hook.end)
-
-    const unregister = () => {
-      if (hook.start) startHooks.delete(hook.start)
-      if (hook.end) endHooks.delete(hook.end)
-    }
-
-    return unregister
+  const enableCallback = () => {
+    if (startHooks.size !== 0) document.body.addEventListener('pointerover', onPointerover)
+    if (endHooks.size !== 0) document.body.addEventListener('pointerout', onPointerout)
   }
+  const disableCallback = () => {
+    requestIdleCallback(() => {
+      if (startHooks.size === 0) document.body.removeEventListener('pointerover', onPointerover)
+      if (endHooks.size === 0) document.body.removeEventListener('pointerout', onPointerout)
+    })
+  }
+
+  return (hooks: { start?: (evt: PointerEvent) => void; end?: (evt: PointerEvent) => void }) => ({
+    enable: () => {
+      if (hooks.start && !startHooks.has(hooks.start)) startHooks.add(hooks.start)
+      if (hooks.end && !endHooks.has(hooks.end)) endHooks.add(hooks.end)
+      enableCallback()
+    },
+    disable: () => {
+      if (hooks.start && startHooks.has(hooks.start)) startHooks.delete(hooks.start)
+      if (hooks.end && endHooks.has(hooks.end)) endHooks.delete(hooks.end)
+      disableCallback()
+    }
+  })
 })()
