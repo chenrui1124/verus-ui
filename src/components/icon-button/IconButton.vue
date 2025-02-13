4f93c4052cc3db2e4db78f60cf8ebeb0d0c9fad1
Rui <75087533+chenrui1124@users.noreply.github.com>
2025-02-13 23:35:43 +0800

    feat: add Icon Button component

---
+++ src/components/icon-button/IconButton.vue
@@ -0,0 +1,44 @@
+<script lang="ts">
+import { Icon } from '@/basic'
+import { useUi } from '@/utils'
+
+export interface IconButtonProps {
+  icon?: string
+  danger?: boolean
+  disabled?: boolean
+}
+</script>
+
+<script lang="ts" setup>
+defineProps<IconButtonProps>()
+
+const emit = defineEmits<{ click: [evt: MouseEvent] }>()
+
+let timeout: number | undefined
+
+function onClick(evt: MouseEvent) {
+  const el = evt.currentTarget as HTMLButtonElement
+  el.classList.add('*:first:scale-75')
+  if (!timeout) {
+    timeout = window.setTimeout(() => {
+      requestAnimationFrame(() => el.classList.remove('*:first:scale-75'))
+      timeout = void 0
+    }, 150)
+  }
+  emit('click', evt)
+}
+</script>
+
+<template>
+  <button
+    @click="onClick"
+    :disabled
+    :data-status="danger ? 'error' : void 0"
+    :class="[
+      'box-border size-9 cursor-pointer rounded-v2 border-none bg-transparent p-2 text-pri transition duration-300 hover:bg-pri/10 focus:bg-pri/20 active:*:first:scale-75',
+      useUi('ring_when_focus_visible')
+    ]"
+  >
+    <Icon v-if="icon" :icon class="size-5 transition duration-300 ease-in-out" />
+  </button>
+</template>
