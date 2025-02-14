def144aa566f28e48274252f6c6afe3403f2cbd5
Rui <75087533+chenrui1124@users.noreply.github.com>
2025-02-14 20:30:18 +0800

    refactor: improved event delegation function

--- src/components/tooltip/useSingleTooltip.tsx
+++ src/components/tooltip/useSingleTooltip.tsx
@@ -2,8 +2,7 @@ import type { Reactive } from 'vue'
 import type { TooltipProps } from '.'
 
 import { reactive } from 'vue'
-import { useRender, useSwitch } from '@/composable'
-import { useHover } from '@/utils'
+import { useListener, useRender, useSwitch } from '@/composable'
 import TooltipContent from './TooltipContent.vue'
 
 export const useSingleTooltip = (() => {
@@ -11,56 +10,6 @@ export const useSingleTooltip = (() => {
   let coord: Reactive<{ x?: string; y?: string }> | null = null
 
   const { state, on, off } = useSwitch()
-
-  const { enable, disable } = useHover({
-    start: evt => {
-      const el = evt.target as HTMLElement
-      const { tooltipSide, tooltipText } = el.dataset
-      if (
-        tooltipSide &&
-        tooltipText &&
-        ['top', 'right', 'bottom', 'left'].includes(tooltipSide) &&
-        props &&
-        coord
-      ) {
-        props.side = tooltipSide as TooltipProps['side']
-        props.text = tooltipText
-        const { top, right, bottom, left } = el.getBoundingClientRect()
-        const center = { y: `${(top + bottom) / 2}px`, x: `${(right + left) / 2}px` }
-        switch (tooltipSide) {
-          case 'top':
-            coord.x = center.x
-            coord.y = `${top}px`
-            break
-          case 'right':
-            coord.x = `${right}px`
-            coord.y = center.y
-            break
-          case 'bottom':
-            coord.x = center.x
-            coord.y = `${bottom}px`
-            break
-          case 'left':
-            coord.x = `${left}px`
-            coord.y = center.y
-            break
-        }
-        on()
-      }
-    },
-    end: () => {
-      if (!state.value) return
-      off()
-      requestAnimationFrame(() => {
-        if (props && coord) {
-          props.side = void 0
-          props.text = void 0
-          coord.x = void 0
-          coord.y = void 0
-        }
-      })
-    }
-  })
 
   const { inc, dec } = useRender(
     () => (
@@ -76,14 +25,68 @@ export const useSingleTooltip = (() => {
       </TooltipContent>
     ),
     {
-      mount: () => {
+      mounted: () => {
         props = reactive({})
         coord = reactive({})
-        enable()
       },
-      unmount: () => disable()
+      unmounted: () => {
+        props = coord = null
+      }
     }
   )
 
-  return () => ({ register: inc, unregister: dec })
+  return () => {
+    useListener(
+      {
+        pointerover: evt => {
+          const el = evt.target as HTMLElement
+          const { tooltipSide, tooltipText } = el.dataset
+          if (
+            tooltipSide &&
+            tooltipText &&
+            ['top', 'right', 'bottom', 'left'].includes(tooltipSide) &&
+            props &&
+            coord
+          ) {
+            props.side = tooltipSide as TooltipProps['side']
+            props.text = tooltipText
+            const { top, right, bottom, left } = el.getBoundingClientRect()
+            const center = { y: `${(top + bottom) / 2}px`, x: `${(right + left) / 2}px` }
+            switch (tooltipSide) {
+              case 'top':
+                coord.x = center.x
+                coord.y = `${top}px`
+                break
+              case 'right':
+                coord.x = `${right}px`
+                coord.y = center.y
+                break
+              case 'bottom':
+                coord.x = center.x
+                coord.y = `${bottom}px`
+                break
+              case 'left':
+                coord.x = `${left}px`
+                coord.y = center.y
+                break
+            }
+            on()
+          }
+        },
+        pointerout: () => {
+          if (!state.value) return
+          off()
+          requestAnimationFrame(() => {
+            if (props && coord) {
+              props.side = void 0
+              props.text = void 0
+              coord.x = void 0
+              coord.y = void 0
+            }
+          })
+        }
+      },
+      { mounted: inc, unmounted: dec }
+    )
+  }
 })()
