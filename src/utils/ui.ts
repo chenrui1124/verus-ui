6b5ec2a09d5e8962186c75c7a3df552320b536e2
Rui <75087533+chenrui1124@users.noreply.github.com>
2025-02-17 20:35:46 +0800

    chore: organize code

--- src/utils/ui.ts
+++ src/utils/ui.ts
@@ -1,13 +1,13 @@
 export const ui = (() => {
   const base = {
-    outline: 'outline-pri/75 outline-offset-3'
-  }
+    outline: 'outline-pri/75 outline-offset-3 outline-2 outline-none'
+  } as const
 
   const each = {
     cover_by_before:
       'before:pointer-events-none before:absolute before:inset-0 before:box-border before:rounded-[inherit]',
-    outline_focus_visible: `focus-visible:outline-2 ${base.outline}`,
-    outline_has_focus_visible: `has-focus-visible:outline-2 ${base.outline}`
+    outline_focus_visible: `focus-visible:outline-solid ${base.outline}`,
+    outline_has_focus_visible: `has-focus-visible:outline-solid ${base.outline}`
   } as const
 
   return <T extends keyof typeof each>(...args: T[]): (typeof each)[T][] =>
