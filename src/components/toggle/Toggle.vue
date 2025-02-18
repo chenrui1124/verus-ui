fbae7e789ab7b7fd95630e1809a1b857c3a1cf52
Rui <75087533+chenrui1124@users.noreply.github.com>
2025-02-18 10:24:24 +0800

    feat: add Toggle component

---
+++ src/components/toggle/Toggle.vue
@@ -0,0 +1,52 @@
+<script lang="ts">
+import { computed } from 'vue'
+import { Icon } from '@/basic'
+import { useSwitch } from '@/composable'
+import { cn, ui } from '@/utils'
+
+export interface ToggleProps {
+  modelValue?: boolean
+  fallbackIcon?: string
+  disabled?: boolean
+  icon?: string
+  rounded?: boolean
+}
+</script>
+
+<script lang="ts" setup>
+const { fallbackIcon, icon } = defineProps<Omit<ToggleProps, 'modelValue'>>()
+
+const modelValue = defineModel<ToggleProps['modelValue']>()
+
+const { state, toggle } = useSwitch({ modelValue })
+
+const rendedIcon = computed(() => (fallbackIcon ? (state.value ? icon : fallbackIcon) : icon))
+</script>
+
+<template>
+  <button
+    @click="toggle"
+    :disabled
+    :class="
+      cn(
+        ui('outline_focus_visible'),
+        'relative h-12 cursor-pointer border-none bg-transparent p-2.5 transition duration-300 disabled:cursor-not-allowed disabled:text-dis',
+        rounded ? 'rounded-full' : 'rounded-v2',
+        state ? 'bg-pri/10 text-pri disabled:bg-dis/30' : 'text-on-sur enabled:hover:bg-on-sur/5'
+      )
+    "
+  >
+    <Transition
+      enter-from-class="opacity-0"
+      leave-to-class="opacity-0"
+      leave-active-class="absolute"
+    >
+      <Icon
+        v-if="rendedIcon"
+        :icon="rendedIcon"
+        :key="rendedIcon"
+        class="size-7 transition duration-300"
+      />
+    </Transition>
+  </button>
+</template>
