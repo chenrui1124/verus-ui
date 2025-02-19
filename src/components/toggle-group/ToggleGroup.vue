f5ba8627450f58415b6fd62d8c57f9e75f5d4362
Rui <75087533+chenrui1124@users.noreply.github.com>
2025-02-19 15:03:58 +0800

    feat: add Toggle Group component

---
+++ src/components/toggle-group/ToggleGroup.vue
@@ -0,0 +1,45 @@
+<script lang="ts">
+import type { DirectionProp } from '@/ts'
+
+import { Icon } from '@/basic'
+import { cn, ui } from '@/utils'
+
+export interface ToggleGroupProps {
+  modelValue?: string
+  direction?: DirectionProp
+  rounded?: boolean
+  items?: { icon: string; value: string }[]
+}
+</script>
+
+<script lang="ts" setup>
+defineProps<Omit<ToggleGroupProps, 'modelValue'>>()
+
+const modelValue = defineModel<ToggleGroupProps['modelValue']>()
+
+function setValue(value: string) {
+  modelValue.value = value
+}
+</script>
+
+<template>
+  <div :class="['box-border inline-flex gap-3', direction === 'column' && 'flex-col']">
+    <button
+      v-for="{ icon, value } in items"
+      :key="value"
+      @click="setValue(value)"
+      :class="
+        cn(
+          ui('outline_focus_visible'),
+          'relative box-border h-10 cursor-pointer border-none bg-transparent p-2 transition duration-300 **:box-border disabled:cursor-not-allowed disabled:text-dis',
+          rounded ? 'rounded-full' : 'rounded-v2',
+          modelValue === value
+            ? 'bg-pri/10 text-pri disabled:bg-dis/30'
+            : 'text-otl enabled:hover:bg-on-sur/5'
+        )
+      "
+    >
+      <Icon :icon class="size-6 transition duration-300" />
+    </button>
+  </div>
+</template>
