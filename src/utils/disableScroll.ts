fa0b13e3c5d4358c870abd803ea5966fed031172
Rui <75087533+chenrui1124@users.noreply.github.com>
2025-02-21 12:36:00 +0800

    refactor: add cache of scrollbar width

--- src/utils/disableScroll.ts
+++ src/utils/disableScroll.ts
@@ -1,31 +1,30 @@
 const getScrollbarWidth = () => {
   const div = document.createElement('div')
-  Object.assign(div.style, {
-    width: '100%',
-    height: '0',
-    position: 'fixed',
-    overflow: 'scroll'
-  })
+  div.style.position = 'fixed'
+  div.style.width = '100%'
+  div.style.height = '0'
+  div.style.overflow = 'scroll'
   document.body.appendChild(div)
   const scrollbarWidth = div.offsetWidth - div.clientWidth
   div.remove()
-  return scrollbarWidth
+  return `${scrollbarWidth}px`
 }
 
 export const disableScroll = (() => {
   let originalOverflow: string | undefined
   let originalPaddingRight: string | undefined
+  let scrollbarWidth: string
 
-  return (newState: boolean | undefined) => {
-    if (newState) {
+  return (disabled: boolean | undefined) => {
+    if (disabled) {
       originalOverflow = document.body.style.overflow
       originalPaddingRight = document.body.style.paddingRight
 
       const paddingRight = getComputedStyle(document.body).paddingRight
-      const scrollbarWidth = getScrollbarWidth()
+      if (!scrollbarWidth) scrollbarWidth = getScrollbarWidth()
 
       document.body.style.overflow = 'hidden'
-      document.body.style.paddingRight = `calc(${paddingRight} + ${scrollbarWidth}px)`
+      document.body.style.paddingRight = `calc(${paddingRight} + ${scrollbarWidth})`
     } else {
       if (typeof originalOverflow === 'string') {
         document.body.style.overflow = originalOverflow
