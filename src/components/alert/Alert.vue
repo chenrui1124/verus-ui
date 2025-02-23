76a85dd36fbfc1602a13e5adb8281274df012335
Rui <75087533+chenrui1124@users.noreply.github.com>
2025-02-23 18:24:43 +0800

    feat: Alert

---
+++ src/components/alert/Alert.vue
@@ -0,0 +1,26 @@
+<script lang="ts">
+import { Icon } from '@/basic'
+
+export interface AlertProps {
+  icon?: string
+  title?: string
+}
+</script>
+
+<script lang="ts" setup>
+defineProps<AlertProps>()
+</script>
+
+<template>
+  <div class="flex gap-3 rounded-v4 bg-pri-ctr px-5 py-3 text-sm/loose text-on-pri-var">
+    <Icon v-if="icon" :icon class="mt-1 size-5 text-pri" />
+    <div class="flex flex-1 flex-col gap-1">
+      <span v-if="title" class="font-semibold text-pri">
+        {{ title }}
+      </span>
+      <p class="m-0 mb-1 p-0">
+        <slot></slot>
+      </p>
+    </div>
+  </div>
+</template>
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
 export * from './accordion'
+export * from './alert'
 export * from './button'
 export * from './dialog'
 export * from './dialog-layout'
