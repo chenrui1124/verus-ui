005011376a5290bdec93ced6085834aa9b30ac91
Rui <75087533+chenrui1124@users.noreply.github.com>
2025-02-27 18:43:11 +0800

    refactor: useResize

--- src/composable/useResize.ts
+++ src/composable/useResize.ts
@@ -1,29 +1,23 @@
 import { onMounted, onUnmounted } from 'vue'
 
-export const useResize = (() => {
-  const cbSet = new Set<Function>()
-  let controller: AbortController | null = null
+function useResizeCreator() {
+  const run = () => {
+    for (const cb of cbs) cb()
+  }
+
+  const cbs = new Proxy(new Set<Function>(), {
+    get(target, prop, receiver) {
+      if (prop === 'size') return target.size
+      if (prop === 'add' && target.size === 0) window.addEventListener('resize', run)
+      if (prop === 'delete' && target.size === 1) window.removeEventListener('resize', run)
+      return Reflect.get(target, prop, receiver).bind(target)
+    }
+  })
 
   return (callback: Function) => {
-    onMounted(() => {
-      cbSet.add(callback)
-      if (cbSet.size === 1) {
-        controller = new AbortController()
-        window.addEventListener(
-          'resize',
-          () => {
-            for (const cb of cbSet) cb()
-          },
-          { signal: controller.signal }
-        )
-      }
-    })
-    onUnmounted(() => {
-      cbSet.delete(callback)
-      if (cbSet.size === 0) {
-        controller?.abort()
-        controller = null
-      }
-    })
+    onMounted(() => cbs.add(callback))
+    onUnmounted(() => cbs.delete(callback))
   }
-})()
+}
+
+export const useResize = useResizeCreator()
