9093d7de6ebb4a819346471446dc166258d0f9d8
Rui <75087533+chenrui1124@users.noreply.github.com>
2025-03-01 16:12:08 +0800

    refactor: improved useRender

--- src/composable/useRender.ts
+++ src/composable/useRender.ts
@@ -2,29 +2,47 @@ import type { App, Component } from 'vue'
 
 import { createApp, onMounted, onUnmounted } from 'vue'
 
-export function useRender(component: Component) {
-  let count = 0
-  let ctr: HTMLDivElement | null = null
-  let app: App | null = null
-
-  onMounted(() => {
-    if (!(ctr || app)) {
-      ctr = document.createElement('div')
-      document.body.appendChild(ctr)
-      app = createApp(component)
-      app.mount(ctr)
-    }
-
-    count++
-  })
-
-  onUnmounted(() => {
-    if (count > 0) count--
-
-    if (!count) {
-      if (app) app.unmount()
-      if (ctr) ctr.remove()
-      ctr = app = null
-    }
-  })
+interface RenderedComponentInfo {
+  container: HTMLDivElement
+  instance: App
+  count: number
 }
+
+function useRenderCreator() {
+  const comps = new WeakMap<Component, RenderedComponentInfo>()
+
+  function register(comp: Component) {
+    const info = comps.get(comp)
+
+    if (info) {
+      info.count++
+    } else {
+      const container = document.createElement('div')
+      document.body.appendChild(container)
+      const instance = createApp(comp)
+      comps.set(comp, { container, instance, count: 1 })
+      instance.mount(container)
+    }
+  }
+
+  function unregister(comp: Component) {
+    const info = comps.get(comp)
+
+    if (info) {
+      info.count--
+      if (info.count <= 0) {
+        info.instance.unmount()
+        info.container.remove()
+        comps.delete(comp)
+      }
+    }
+  }
+
+  return (component: Component) => {
+    onMounted(() => register(component))
+
+    onUnmounted(() => unregister(component))
+  }
+}
+
+export const useRender = useRenderCreator()
