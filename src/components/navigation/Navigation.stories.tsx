ea787bb06dc1701c73db19a6fbb7a8e05ca631b0
Rui <75087533+chenrui1124@users.noreply.github.com>
2025-03-02 14:18:08 +0800

    refactor(Navigation): styles

--- src/components/navigation/Navigation.stories.tsx
+++ src/components/navigation/Navigation.stories.tsx
@@ -1,7 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/vue3'
 
 import { fn } from '@storybook/test'
-import { Random } from 'mockjs'
 import { VNavigation } from '.'
 
 const meta: Meta<typeof VNavigation> = {
@@ -11,17 +10,12 @@ const meta: Meta<typeof VNavigation> = {
     modelValue: { control: 'text' },
     items: { control: 'object' }
   },
-  args: {
-    items: [
-      { text: Random.ctitle(), icon: 'i-[fluent--circle-24-filled]', value: Random.guid() },
-      { text: Random.ctitle(), value: Random.guid() },
-      { text: Random.ctitle(), value: Random.guid() },
-      { text: Random.ctitle(), value: Random.guid() }
-    ],
-    onPush: fn()
-  },
   render(args) {
-    return () => <VNavigation {...args}></VNavigation>
+    return () => (
+      <div class='w-60'>
+        <VNavigation {...args}></VNavigation>
+      </div>
+    )
   }
 }
 
@@ -29,4 +23,46 @@ export default meta
 
 type Story = StoryObj<typeof meta>
 
-export const Default: Story = {}
+export const Default: Story = {
+  args: {
+    items: [
+      { text: '链接 1', value: '1', icon: 'i-[fluent--square-24-filled]' },
+      {
+        text: '分组 1',
+        group: [
+          { text: '链接 2', value: '1-1', icon: 'i-[fluent--square-24-filled]' },
+          { text: '链接 3', value: '1-2' }
+        ],
+        icon: 'i-[fluent--square-24-filled]'
+      },
+      {
+        text: '分组 2',
+        group: [
+          { text: '链接 2', value: '2-1' },
+          { text: '链接 3', value: '2-2' }
+        ]
+      },
+      { text: '链接 2', value: '2' }
+    ],
+    onPush: fn()
+  }
+}
+
+export const Example: Story = {
+  args: {
+    items: [
+      { text: 'Bookmark', value: 'bookmark', icon: 'i-[fluent--bookmark-24-filled]' },
+      { text: 'Introduction', value: 'introduction', icon: 'i-[fluent--slide-text-24-filled]' },
+      {
+        text: 'Contents',
+        group: [
+          { text: 'Section 1', value: 'section 1' },
+          { text: 'Section 2', value: 'section 2' }
+        ],
+        icon: 'i-[fluent--text-column-wide-24-filled]'
+      },
+      { text: 'About', value: 'about' }
+    ],
+    onPush: fn()
+  }
+}
--- src/components/navigation/Navigation.vue
+++ src/components/navigation/Navigation.vue
@@ -1,7 +1,22 @@
 <script lang="ts">
+import type { Either } from 'mm2r'
+
 import { BaseIcon } from '@/base'
 
-type NavigationPropsItem = { text: string; icon?: string; value: string }
+type NavigationPropsItemDefault = {
+  text: string
+  value: string
+  icon?: string
+}
+
+type NavigationPropsItemGroup = {
+  text: string
+  open?: boolean
+  group: NavigationPropsItemDefault[]
+  icon?: string
+}
+
+type NavigationPropsItem = Either<NavigationPropsItemDefault, NavigationPropsItemGroup>
 
 export interface NavigationProps {
   modelValue?: string
@@ -27,22 +42,64 @@ function setModelValue(evt: MouseEvent) {
   <div
     v-if="items"
     @click="setModelValue"
-    class="box-border grid grid-cols-[min-content_1fr] flex-col gap-y-1 **:box-border"
+    class="box-border grid grid-cols-[min-content_1fr_min-content] space-y-1 text-sm/loose text-on-sur select-none **:box-border"
   >
-    <span
-      v-for="({ text, icon, value }, key) in items"
-      :key
-      tabindex="0"
-      :data-value="value"
-      :class="[
-        'col-span-2 grid cursor-pointer grid-cols-subgrid items-center rounded-v2 border-none px-4 text-left text-sm/9 no-underline transition duration-300',
-        modelValue === value
-          ? 'pointer-events-none bg-pri-ctr text-pri'
-          : 'bg-transparent text-on-sur hover:bg-on-sur/5'
-      ]"
-    >
-      <BaseIcon v-if="icon" :icon class="mr-3 -ml-1 size-5" />
-      <span class="pointer-events-none col-start-2">{{ text }} </span>
-    </span>
+    <template v-for="(first, key) of items" :key>
+      <div
+        v-if="first.group"
+        class="col-span-3 grid grid-cols-subgrid grid-rows-[min-content_0fr] transition-all duration-300 ease-braking has-checked:grid-rows-[min-content_1fr]"
+      >
+        <label
+          class="peer col-span-3 grid h-8 cursor-pointer grid-cols-subgrid items-center rounded-v1 px-3 font-semibold text-otl transition duration-300 hover:bg-sur-var hover:text-inherit has-checked:text-inherit"
+        >
+          <input
+            type="checkbox"
+            :checked="first.open"
+            class="peer pointer-events-none col-start-3 row-start-1 m-0 -mr-0.5 ml-1 i-[fluent--caret-right-24-filled] size-4 appearance-none p-0 text-inherit transition duration-300 ease-braking checked:rotate-90"
+          />
+          <span
+            class="pointer-events-none col-start-2 row-start-1 transition duration-300 ease-braking peer-checked:-translate-x-6.25"
+          >
+            {{ first.text }}
+          </span>
+          <BaseIcon
+            v-if="first.icon"
+            :icon="first.icon"
+            class="col-start-1 row-start-1 mr-2 -ml-0.5 transition duration-300 ease-braking peer-checked:scale-0"
+          />
+        </label>
+        <div
+          class="col-span-3 mt-0 ml-5 grid grid-cols-[min-content_1fr] space-y-1 overflow-y-hidden border-l border-l-otl-var pl-4 transition-all duration-300 peer-has-checked:mt-1"
+        >
+          <div
+            v-for="(second, key) of first.group"
+            :key
+            :data-value="second.value"
+            :class="[
+              'col-span-2 grid h-8 cursor-pointer grid-cols-subgrid items-center rounded-v1 px-3 transition duration-300',
+              modelValue === second.value
+                ? 'pointer-events-none bg-pri-ctr text-pri'
+                : 'text-otl hover:bg-sur-var hover:text-inherit'
+            ]"
+          >
+            <BaseIcon v-if="second.icon" :icon="second.icon" class="col-start-1 mr-2 -ml-0.5" />
+            <span class="pointer-events-none col-start-2 ml-px">{{ second.text }}</span>
+          </div>
+        </div>
+      </div>
+      <div
+        v-else
+        :data-value="first.value"
+        :class="[
+          'col-span-3 grid h-8 cursor-pointer grid-cols-subgrid items-center rounded-v1 px-3 transition duration-300',
+          modelValue === first.value
+            ? 'pointer-events-none bg-pri-ctr text-pri'
+            : 'text-otl hover:bg-sur-var hover:text-inherit'
+        ]"
+      >
+        <BaseIcon v-if="first.icon" :icon="first.icon" class="col-start-1 mr-2 -ml-0.5" />
+        <span class="pointer-events-none col-start-2">{{ first.text }}</span>
+      </div>
+    </template>
   </div>
 </template>
