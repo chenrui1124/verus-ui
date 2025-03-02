8857554f2d83f2430dd292c9c64b083b6f33d252
Rui <75087533+chenrui1124@users.noreply.github.com>
2025-03-02 10:27:58 +0800

    feat(Tabs): update styles & props

--- src/components/tabs/Tabs.stories.tsx
+++ src/components/tabs/Tabs.stories.tsx
@@ -7,30 +7,32 @@ const meta: Meta<typeof VTabs<any>> = {
   component: VTabs as any,
   argTypes: {
     modelValue: { control: 'text' },
-    items: { control: 'object' }
-  },
-  args: {
-    modelValue: 'windows',
-    items: [
-      { text: 'Windows', value: 'windows' },
-      { text: 'Linux', value: 'linux' },
-      { text: 'MacOS', value: 'macos' }
-    ] as const
-  },
-  render: args => () => (
-    <VTabs
-      {...args}
-      v-slots={{
-        windows: () => <div>Download for Windows</div>,
-        linux: () => <div>Download for Linux</div>,
-        macos: () => <div>Download for MacOS</div>
-      }}
-    />
-  )
+    items: { control: 'object' },
+    stretch: { control: 'boolean' },
+    uniformWidth: { control: 'boolean' }
+  }
 }
 
 export default meta
 
 type Story = StoryObj<typeof meta>
 
-export const Default: Story = {}
+export const Default: Story = {
+  args: {
+    modelValue: 'windows',
+    items: Array.from({ length: 3 }).map((_, i) => ({
+      text: `标签 ${i + 1}`,
+      value: `tab${i + 1}`
+    }))
+  },
+  render: args => () => (
+    <VTabs
+      {...args}
+      v-slots={{
+        tab1: () => <div class='text-sm'>内容 1</div>,
+        tab2: () => <div class='text-sm'>内容 2</div>,
+        tab3: () => <div class='text-sm'>内容 3</div>
+      }}
+    />
+  )
+}
--- src/components/tabs/Tabs.vue
+++ src/components/tabs/Tabs.vue
@@ -1,6 +1,7 @@
 <script lang="ts">
 import type { MaybeReadonly } from 'mm2r'
 
+import { computed } from 'vue'
 import { ui } from '@/utils'
 
 type TabsPropItem = { text: string; value: string }
@@ -8,6 +9,8 @@ type TabsPropItem = { text: string; value: string }
 export interface TabsProps {
   modelValue?: string
   items?: MaybeReadonly<TabsPropItem[]>
+  stretch?: boolean
+  uniformWidth?: boolean
 }
 
 export interface TabsSlots {
@@ -16,44 +19,65 @@ export interface TabsSlots {
 </script>
 
 <script lang="ts" setup generic="T extends TabsPropItem">
-defineProps<Omit<TabsProps & { items?: MaybeReadonly<T[]> }, 'modelValue'>>()
+interface TabsGenericProps extends TabsProps {
+  items: MaybeReadonly<T[]>
+}
+
+const { items } = defineProps<Omit<TabsGenericProps, 'modelValue'>>()
 
 const modelValue = defineModel<T['value']>()
 
-const slots = defineSlots<{ [S in T['value']]?: () => any }>()
+const index = computed(() => items?.findIndex(({ value }) => value === modelValue.value))
 
-const TabsView = () =>
-  modelValue.value && modelValue.value in slots && slots[modelValue.value as keyof typeof slots]!()
+function setModelValue(value: string) {
+  modelValue.value = value
+}
+
+const slots = defineSlots<{ [S in T['value']]+?: () => any }>()
+
+const TabsView = ({ name }: { name: string }) => {
+  if (!name) return null
+  const currentSlot = slots[name as keyof typeof slots]
+  if (currentSlot) return currentSlot()
+  return null
+}
 </script>
 
 <template>
-  <div class="box-border flex flex-col gap-1" v-if="items">
-    <div class="box-border flex gap-1 p-1">
-      <button
-        v-for="({ text, value }, key) of items"
-        :key
-        @click="modelValue = value"
-        :class="[
-          'h-8 cursor-pointer rounded-v1 border-none px-4 text-sm/8 transition duration-300',
-          modelValue === value
-            ? 'pointer-events-none bg-pri-ctr text-pri'
-            : 'bg-transparent text-on-sur hover:bg-on-sur/5',
-          ui('outline_focus_visible')
-        ]"
+  <div v-if="items" class="box-border flex flex-col gap-3 p-1.5">
+    <div :class="['flex', stretch && 'flex-col']">
+      <div
+        :style="{
+          gridTemplateColumns: `repeat(${items.length}, ${uniformWidth ? '1fr' : 'auto'})`
+        }"
+        class="relative grid gap-1.5 rounded-v1"
       >
-        {{ text }}
-      </button>
+        <button
+          v-for="({ text, value }, key) of items"
+          :key
+          @click="setModelValue(value)"
+          :class="[
+            'h-8 cursor-pointer rounded-[inherit] border-none px-4 text-sm/8 transition duration-300',
+            modelValue === value
+              ? 'pointer-events-none bg-pri-ctr text-pri'
+              : 'bg-transparent text-on-sur hover:bg-on-sur/5',
+            ui('outline_focus_visible')
+          ]"
+        >
+          {{ text }}
+        </button>
+      </div>
     </div>
-    <div class="relative p-3 text-sm text-on-sur">
-      <Transition
-        enter-from-class="opacity-0"
-        leave-to-class="opacity-0"
-        leave-active-class="absolute"
+
+    <div class="overflow-x-hidden">
+      <div
+        :style="{ transform: index ? `translateX(-${index * 100}%)` : void 0 }"
+        class="flex w-full flex-nowrap transition duration-300"
       >
-        <div :key="modelValue" class="transition duration-300">
-          <component :is="TabsView"></component>
+        <div v-for="{ value } of items" class="min-w-full">
+          <TabsView :name="value" />
         </div>
-      </Transition>
+      </div>
     </div>
   </div>
 </template>
