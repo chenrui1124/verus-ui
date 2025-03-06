47771f6fb8f8f876c666d48b5aff6d64af48c9ef
Rui <75087533+chenrui1124@users.noreply.github.com>
2025-03-06 20:24:39 +0800

    refactor(Switch): update styles, improve accessibility

--- src/components/switch/Switch.stories.tsx
+++ src/components/switch/Switch.stories.tsx
@@ -24,3 +24,16 @@ export default meta
 type Story = StoryObj<typeof meta>
 
 export const Default: Story = {}
+
+export const Example: Story = {
+  render(args) {
+    return () => (
+      <div class='flex w-60 flex-col rounded-v3 border border-otl-var p-6'>
+        <label class='flex items-center gap-3'>
+          <span class='flex-1 text-sm text-on-sur'>飞行模式</span>
+          <VSwitch {...args} />
+        </label>
+      </div>
+    )
+  }
+}
--- src/components/switch/Switch.vue
+++ src/components/switch/Switch.vue
@@ -1,6 +1,7 @@
 <script lang="ts">
 import type { StatusProp } from '@/ts'
 
+import { useSwitch } from '@/composable'
 import { cn, ui } from '@/utils'
 
 export interface SwitchProps {
@@ -17,29 +18,30 @@ export interface SwitchProps {
 defineProps<Omit<SwitchProps, 'modelValue'>>()
 
 const modelValue = defineModel<SwitchProps['modelValue']>()
+
+const { state, toggle } = useSwitch({ modelValue })
 </script>
 
 <template>
-  <label
+  <button
+    :disabled
+    role="switch"
+    :aria-checked="state"
     :data-status="status"
+    @click="toggle"
     :class="
       cn(
-        'group/switch relative box-border inline-block h-7 w-12 cursor-pointer rounded-full bg-sur transition duration-300 **:box-border',
-        'before:border before:border-solid before:border-otl before:bg-otl/20 before:transition before:duration-300 has-checked:before:border-transparent has-checked:before:bg-pri',
-        'has-disabled:cursor-not-allowed has-disabled:opacity-30 has-disabled:before:border-dis has-disabled:before:bg-dis',
-        '*:top-1/2 *:left-3.5 has-checked:*:translate-x-5',
-        ui('cover_by_before', 'outline_has_focus_visible')
+        'group/switch relative box-border inline-block h-7 w-12 cursor-pointer rounded-full border-none bg-sur transition duration-300 **:box-border',
+        'before:border before:border-solid before:border-otl before:bg-otl/20 before:transition before:duration-300 aria-checked:before:border-transparent enabled:aria-checked:before:bg-pri',
+        'disabled:cursor-not-allowed disabled:before:border-dis disabled:before:bg-dis',
+        ui('cover_by_before', 'outline_focus_visible'),
+        'after:absolute after:top-1/2 after:left-3.5 after:-m-4.5 after:size-9 after:rounded-full after:transition after:duration-300 after:ease-braking hover:after:bg-on-sur/10 disabled:after:opacity-0 aria-checked:after:translate-x-5 aria-checked:hover:after:bg-pri/10'
       )
     "
   >
-    <input
-      type="checkbox"
-      :disabled
-      v-model="modelValue"
-      class="peer pointer-events-none absolute -m-2.5 inline-block size-5 appearance-none rounded-full bg-otl transition duration-300 ease-braking outline-none not-checked:scale-75 group-active/switch:scale-125 checked:bg-sur"
-    />
     <span
-      class="pointer-events-none absolute -m-4.5 inline-block size-9 rounded-full transition duration-300 ease-braking group-hover/switch:bg-on-sur/10 group-hover/switch:peer-checked:bg-pri/10 peer-disabled:hidden"
+      aria-hidden="true"
+      class="pointer-events-none absolute top-1/2 left-3.5 -m-2.5 inline-block size-5 rounded-full bg-otl drop-shadow-sm transition duration-300 ease-braking outline-none group-not-aria-checked/switch:scale-75 group-disabled/switch:bg-on-dis group-aria-checked/switch:translate-x-5 group-aria-checked/switch:bg-sur group-[:enabled:active]/switch:scale-125"
     ></span>
-  </label>
+  </button>
 </template>
