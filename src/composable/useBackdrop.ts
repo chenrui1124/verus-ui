29f60ec7706950bc08ff106d474b4c702fc96725
Rui <75087533+chenrui1124@users.noreply.github.com>
2025-03-11 12:28:02 +0800

    fix: prevent multiple renders in useRender

--- src/composable/useBackdrop.ts
+++ src/composable/useBackdrop.ts
@@ -1,15 +1,18 @@
 import type { ComputedRef, Ref } from 'vue'
 
-import { computed, h, onScopeDispose, reactive } from 'vue'
+import { computed, h, onScopeDispose, reactive, ref } from 'vue'
 import { BaseBackdrop } from '@/base'
 import { useRender } from '@/composable'
 
 export const useBackdrop = (() => {
   let states: Ref<boolean | undefined>[] | null
   let active: ComputedRef<boolean | undefined> | null
+  let className: Ref<string | undefined> | null
+
+  const BackdropWrapper = () => h(BaseBackdrop, { class: className, state: active!.value })
 
   const dispose = () => {
-    if (states && states.length === 0) states = active = null
+    if (states && states.length === 0) states = active = className = null
   }
 
   return (
@@ -21,13 +24,20 @@ export const useBackdrop = (() => {
       active = computed(() => states!.some(ref => ref.value))
     }
     states.push(state)
-    useRender(() => h(BaseBackdrop, { class: additionalClass, state: active!.value }))
+
+    if (additionalClass) {
+      if (!className) className = ref()
+      className.value = additionalClass
+    }
+
+    useRender(BackdropWrapper)
 
     onScopeDispose(() => {
       if (states) {
         states.splice(states.indexOf(state), 1)
         dispose()
       }
+      if (className) className.value = void 0
     })
   }
 })()
